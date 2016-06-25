09926e109147c77995c077ba75cc7b529b789832
kpcyrd <git@rxv.cc>
2016-06-25 07:35:13 +0200

    Add ipfs-mirror empty

--- ipfs-mirror.py
+++ ipfs-mirror.py
@@ -15,7 +15,8 @@ def ipfs(cmd):
     return output
 
 
-def ipfs_new_dir():
+def empty():
+    'Create empty folder'
     return ipfs(['object', 'new', 'unixfs-dir'])
 
 
@@ -24,6 +25,7 @@ def ipfs_add(path):
 
 
 def add(path):
+    'Get ipfs path for file'
     # TODO: caching
     multihash = ipfs_add(path)
     log('[+] added %r -> %s' % (path, multihash))
@@ -31,6 +33,7 @@ def add(path):
 
 
 def merge(root, name, multihash):
+    'Merge folder into another folder'
     return ipfs(['object', 'patch', root, 'add-link', name, multihash])
 
 
@@ -45,7 +48,7 @@ def process_folder(root, files):
 
 
 def ipfs_patch_dir(content):
-    folder = ipfs_new_dir()
+    folder = empty()
     for name, multihash in content.items():
         folder = merge(folder, name, multihash)
     return folder
@@ -64,6 +67,7 @@ def resolve(root, tree):
 
 
 def mirror(folder):
+    'Mirror a folder'
     tree = {}
     for root, subs, files in os.walk(folder):
         folder_content = process_folder(root, files)
@@ -78,7 +82,7 @@ def mirror(folder):
 
 def main():
     parser = ArghParser(description='todo')
-    parser.add_commands([mirror, add, merge])
+    parser.add_commands([mirror, add, empty, merge])
     dispatch(parser)
 
 if __name__ == '__main__':
